786c7e2d44c418891e24062f1c6e9d0f7863ee8b
ThibaudP <t.poncin@gmail.com>
2021-05-03 14:14:20 +0000

    fix!

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,26 +5,33 @@ import requests
 from sys import argv
 
 
+def main():
+    """main function"""
+    if len(argv) < 2:
+        return
+
+    userUrl = "https://jsonplaceholder.typicode.com/users/{}".format(argv[1])
+    todoUrl = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+        argv[1])
+
+    userReq = requests.get(userUrl)
+    user = json.loads(userReq.text)
+    todoReq = requests.get(todoUrl)
+    todo = json.loads(todoReq.text)
+
+    numTasks = len(todo)
+    numComplete = 0
+    for task in todo:
+        if task['completed']:
+            numComplete += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(user['name'],
+                                                          numComplete,
+                                                          numTasks))
+    for task in todo:
+        if task['completed']:
+            print("\t {}".format(task['title']))
+
+
 if __name__ == "__main__":
-    if len(argv) == 2:
-        n = argv[1]
-
-        userUrl = "https://jsonplaceholder.typicode.com/users/{}".format(n)
-        todoUrl = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-            n)
-
-        userReq = requests.get(userUrl)
-        user = userReq.json()
-        todoReq = requests.get(todoUrl)
-        todo = todoReq.json()
-
-        completed = []
-        for task in todo:
-            if task.get('completed'):
-                completed.append(task)
-
-        print("Employee {} is done with tasks({}/{}):".format(user.get('name'),
-                                                             len(completed),
-                                                             len(todo)))
-        for task in completed:
-            print("\t {}".format(task.get('title')))
+    main()
