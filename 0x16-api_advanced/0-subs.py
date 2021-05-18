b65fd987266dd4cf7e660f92fac941833b439b40
ThibaudP <t.poncin@gmail.com>
2021-05-18 21:51:03 +0000

    fix?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,7 +3,6 @@
 0-subs
 """
 import requests
-import json
 
 
 def number_of_subscribers(subreddit):
@@ -12,14 +11,9 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "ubuntu:hbtn:v1.0 (by /u/eskaps)"}
 
-    # print("{}".format(url))
-
     request = requests.get(url, headers=headers, allow_redirects=False)
 
-    # print(request.json().get("data"))
-    # print(json.dumps(json.loads(request.text).get("data"), indent=2))
+    if request.status_code == 404:
+        return 0
 
-    if request.status_code == 200:
-        return request.json().get("data").get("subscribers")
-
-    return 0
+    return request.json().get("data").get("subscribers")
